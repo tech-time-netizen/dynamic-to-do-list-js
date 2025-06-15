c4ec23bacf7368959c471f7c22e5a28b4b1544eb
unknown <e2476861@gmail.com>
2025-06-15 15:12:55 +0300

    Add the remove-btn classList instead of className

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const removeBtn = document.createElement('button');
         removeBtn.textContent = 'Remove';
-        removeBtn.className = 'remove-btn';
+        removeBtn.classList.add('remove-btn');
         removeBtn.addEventListener('click', function () {
             taskList.removeChild(li);
         });
